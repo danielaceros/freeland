9d5df10fc1a2e2ad29bc977c1a67324f55bf8a7f
danielaceros <danielacerosagredos@gmail.com>
2024-10-24 18:30:13 +0200

    24

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
       <div className="flex-1 flex flex-col">
         {/* Header */}
         <header className="flex justify-between items-center bg-white p-4 shadow">
-          <h2 className="text-3xl font-semibold">Overview</h2>
+          <h2 className="text-3xl font-semibold">Overviews</h2>
           <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-500">
             Add New
           </button>
