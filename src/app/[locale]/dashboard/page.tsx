3f92c43e8dd4712757652a148512a3912cb22c78
lobo772 <59614268+lobo772@users.noreply.github.com>
2024-11-07 10:21:32 +0100

    fix: diseño perfil, menú y refactor

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,28 +1,16 @@
-"use client";
+'use client';
+
+import type { User } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth'; // Import onAuthStateChanged and User
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation'; // Import useRouter from Next.js
+
+import Menu from '@/components/common/Menu';
+
 import { auth } from '../../../libs/firebase'; // Ensure this path is correct to your Firebase setup
-import { signOut } from 'firebase/auth'; // Import signOut method
-import { toast } from 'react-toastify';
-import { onAuthStateChanged, User } from 'firebase/auth'; // Import onAuthStateChanged and User
 
 export default function Dashboard() {
-  const router = useRouter(); // Get router instance
   const [user, setUser] = useState<User | null>(null); // Specify the state type as User or null
   const [loading, setLoading] = useState(true); // Loading state
-
-
-  // Function to handle logout
-  const handleLogout = async () => {
-    try {
-      await signOut(auth); // Sign out the user
-      toast.success("¡Adios!, " + user?.email);
-      router.push('/'); // Redirect to home page after logout
-    } catch (error) {
-      console.error("Error al cerrar sesión:", error);
-      toast.error("Error al cerrar sesión."); // Show error message
-    }
-  };
 
   // Effect to retrieve current user information
   useEffect(() => {
@@ -40,49 +28,23 @@ export default function Dashboard() {
 
   // Loading Spinner Component
   const LoadingSpinner = () => (
-    <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-600 border-b-4"></div>
+    <div className="flex h-full items-center justify-center">
+      <div className="size-16 animate-spin rounded-full border-y-4 border-green-600" />
     </div>
   );
 
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Left sidebar */}
-      <aside className="w-64 bg-green-800 text-white p-4">
-        {user ? ( // Check if the user is available
-          <h1 className="text-xl font-bold mb-6">¡Welcome, {user.email?.split("@")[0]}!</h1>
-        ) : (
-          <h1 className="text-xl font-bold mb-6"></h1>
-        )} {/* Display user's email */}
-        <nav>
-        <ul>
-            <li className="px-4 py-2 hover:bg-green-700 cursor-pointer bg-green-600" onClick={() => router.push('/en/dashboard')}>
-              Dashboard
-            </li>
-            <li className="px-4 py-2 hover:bg-green-700 cursor-pointer" onClick={() => router.push('/en/dashboard/hire')}>
-              Hire
-            </li>
-            <li className="px-4 py-2 hover:bg-green-700 cursor-pointer" onClick={() => router.push('/en/dashboard/work')}>
-              Work
-            </li>
-            <li className="px-4 py-2 hover:bg-green-700 cursor-pointer" onClick={() => router.push('/en/dashboard/profile')}>
-              Profile
-            </li>
-            <li className="px-4 py-2 hover:bg-green-700 cursor-pointer" onClick={handleLogout}>
-              Logout
-            </li>
-          </ul>
-        </nav>
-      </aside>
+      <Menu />
 
       {/* Main content */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex flex-1 flex-col">
         {/* Header */}
-        <header className="flex justify-between items-center bg-white p-4 shadow">
+        <header className="flex items-center justify-between bg-white p-4 shadow">
           <h2 className="text-3xl font-semibold">Dashboard</h2>
-          <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-500">
+          <div className="rounded bg-green-600 px-4 py-2 text-white hover:bg-green-500">
             Add New
-          </button>
+          </div>
         </header>
 
         {/* Content area */}
@@ -91,33 +53,31 @@ export default function Dashboard() {
             <LoadingSpinner />
           ) : (
             <div>
-              {user ? ( // Show user information if available
+              {user && ( // Show user information if available
                 <div className="mb-6">
-                  <h3 className="text-xl font-semibold">¡Welcome, {user.displayName || user.email}!</h3>
-                </div>
-              ) : (
-                <div className="mb-6">
-                  <h3 className="text-xl font-semibold"></h3>
+                  <h3 className="text-xl font-semibold">
+                    ¡Welcome, {user.displayName || user.email}!
+                  </h3>
                 </div>
               )}
 
-              <div className="grid grid-cols-3 gap-6 mb-6">
+              <div className="mb-6 grid grid-cols-3 gap-6">
                 {/* Card 1 */}
-                <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+                <div className="rounded-lg bg-white p-6 shadow-md transition-shadow hover:shadow-xl">
                   <h3 className="text-lg font-semibold">Total Sales</h3>
                   <p className="text-3xl text-green-600">$10,000</p>
                   <p className="text-gray-600">+15% from last month</p>
                 </div>
 
                 {/* Card 2 */}
-                <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+                <div className="rounded-lg bg-white p-6 shadow-md transition-shadow hover:shadow-xl">
                   <h3 className="text-lg font-semibold">Active Users</h3>
                   <p className="text-3xl text-green-600">1,250</p>
                   <p className="text-gray-600">+30% from last month</p>
                 </div>
 
                 {/* Card 3 */}
-                <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+                <div className="rounded-lg bg-white p-6 shadow-md transition-shadow hover:shadow-xl">
                   <h3 className="text-lg font-semibold">New Signups</h3>
                   <p className="text-3xl text-green-600">300</p>
                   <p className="text-gray-600">+20% from last month</p>
@@ -126,7 +86,7 @@ export default function Dashboard() {
 
               <div className="grid grid-cols-2 gap-6">
                 {/* Card 4 */}
-                <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+                <div className="rounded-lg bg-white p-6 shadow-md transition-shadow hover:shadow-xl">
                   <h3 className="text-lg font-semibold">Latest Activity</h3>
                   <ul className="mt-2">
                     <li className="text-gray-700">User John signed up</li>
@@ -137,12 +97,15 @@ export default function Dashboard() {
                 </div>
 
                 {/* Card 5 */}
-                <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow">
+                <div className="rounded-lg bg-white p-6 shadow-md transition-shadow hover:shadow-xl">
                   <h3 className="text-lg font-semibold">Revenue Overview</h3>
-                  <p className="text-green-600 text-3xl">$2,500</p>
+                  <p className="text-3xl text-green-600">$2,500</p>
                   <p className="text-gray-600">This week</p>
                   <div className="mt-4">
-                    <div className="bg-green-200 h-4 rounded" style={{ width: '75%' }}></div>
+                    <div
+                      className="h-4 rounded bg-green-200"
+                      style={{ width: '75%' }}
+                    />
                   </div>
                 </div>
               </div>
---
+++ src/components/common/Menu.tsx
@@ -0,0 +1,167 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { useUser } from '@/hooks/useUser';
+import { auth } from '@/libs/firebase';
+
+import { Logo } from './Logo';
+
+const Menu = () => {
+  const { profileData } = useUser();
+  const userName = profileData.name;
+  const lastName = profileData.surname;
+  const img = profileData.profilePicture;
+  const [isOpen, setIsOpen] = useState(true);
+  const router = useRouter(); // Get router instance
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      toast.success('Logged out successfully!'); // Notify on successful logout
+      router.push('/'); // Redirect to home page after logout
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
+  return (
+    <aside
+      className={`${
+        isOpen ? 'w-64' : 'w-20'
+      } flex flex-col bg-zinc-800 p-4 text-white transition-all duration-100`}
+    >
+      <button
+        type="button"
+        className="flex justify-end p-4 focus:outline-none"
+        onClick={toggleSidebar}
+      >
+        {/* Icono del menú */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="size-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      </button>
+
+      <button
+        type="button"
+        className="flex cursor-pointer justify-center"
+        onClick={() => router.push('/dashboard')}
+      >
+        <Logo />
+      </button>
+      <hr className="my-7 border-zinc-700" />
+      <button
+        type="button"
+        className="mb-5 flex cursor-pointer"
+        onClick={() => router.push('/dashboard/profile')}
+      >
+        {img && (
+          <img
+            src={img}
+            alt="Ver Perfil"
+            className={`${isOpen ? 'size-16' : 'size-12'} rounded-full border-2 border-green-600`}
+          />
+        )}
+        {isOpen && (
+          <div className="ml-3 text-freeland">
+            <p className="text-xl font-bold">{userName}</p>
+            <p className="text-md font-bold">{lastName}</p>
+          </div>
+        )}
+      </button>
+      <nav className="flex flex-col items-start pl-3">
+        <button
+          type="button"
+          className="cursor-pointer py-2 hover:text-freeland"
+          onClick={() => router.push('/dashboard/')}
+        >
+          {isOpen ? (
+            'Dashboard'
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          )}
+        </button>
+        <button
+          type="button"
+          className="cursor-pointer py-2 hover:text-freeland"
+          onClick={() => router.push('/dashboard/hire')}
+        >
+          {isOpen ? (
+            'Crear nueva oferta'
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          )}
+        </button>
+        <button
+          type="button"
+          className="cursor-pointer py-2 hover:text-freeland"
+          onClick={handleLogout}
+        >
+          {isOpen ? (
+            'Logout'
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+              />
+            </svg>
+          )}
+        </button>
+      </nav>
+    </aside>
+  );
+};
+
+export default Menu;
